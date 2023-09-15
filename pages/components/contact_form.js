be5cc34bbe300a7c7f0e3ae71f6dcaeee6361aff
jackjk521 <jedchu541@gmail.com>
2023-09-16 01:15:31 +0800

    Applied sequelize to the contact form

--- pages/components/contact_form.js
+++ pages/components/contact_form.js
@@ -1,29 +1,52 @@
-import React from 'react';
-import { makeStyles } from '@mui/styles';
-import {
-  Container,
-  Typography,
-  TextField,
-  Button,
-  Grid,
-} from '@mui/material';
+import React, { useState } from "react";
+import { makeStyles } from "@mui/styles";
+import { Container, Typography, TextField, Button, Grid } from "@mui/material";
 
 const ContactForm = () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Retrieve form data
+    const formData = new FormData(e.target);
+    const user_name = formData.get("name");
+    const user_email = formData.get("email");
+    const message = formData.get("message");
+
+    const contactData = {
+      user_name,
+      user_email,
+      message,
+    };
+
+    try {
+      const response = await fetch("/api/contact", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(contactData),
+      });
+
+      if (response.ok) {
+        // Contact successfully submitted
+        // Do something with the response if needed
+        console.log("sucess");
+      } else {
+        // Handle error response
+      }
+    } catch (error) {
+      // Handle network or other errors
+    }
+  };
 
   return (
-    <Container component="main" maxWidth="xs" sx={{padding: 4  }}>
+    <Container component="main" maxWidth="xs" sx={{ padding: 4 }}>
       {/* <Typography variant="subtitle" textAlign="center">
         Interested on my services and want to reach out then here is your chance
       </Typography> */}
-      <form >
+      <form onSubmit={handleSubmit}>
         <Grid container spacing={2}>
           <Grid item xs={12}>
-            <TextField
-              variant="outlined"
-              fullWidth
-              label="Name"
-              name="name"
-            />
+            <TextField variant="outlined" fullWidth label="Name" name="name" />
           </Grid>
           <Grid item xs={12}>
             <TextField
@@ -50,8 +73,7 @@ const ContactForm = () => {
           fullWidth
           variant="contained"
           color="primary"
-          sx={{marginTop:2}}
-        >
+          sx={{ marginTop: 2 }}>
           Send
         </Button>
       </form>
